6a1d2dadc16b049a1af8e7f10f28fbd14cac6109
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-08 16:11:52 -0500

    Creation of save, delete and list methods

---
+++ src/main/java/com/example/crudi/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudi.repository;
+
+import com.example.crudi.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ src/main/java/com/example/crudi/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.example.crudi.service;
+
+import com.example.crudi.entity.Student;
+import com.example.crudi.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudent(Long id){
+        return studentRepository.findById(id);
+    }
+
+    public void saveOrUpdate(Student student){
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id){
+        studentRepository.deleteById(id);
+    }
+}
